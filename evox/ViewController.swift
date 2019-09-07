0d550e9bd9f432515465a4283d40d332dc6dba12
John Garrett <jgarrett@kabbage.com>
2019-09-07 00:51:28 -0400

    inital commit

--- evox/ViewController.swift
+++ evox/ViewController.swift
@@ -1,20 +1,8 @@
-//
-//  ViewController.swift
-//  evox
-//
-//  Created by John Garrett on 9/7/19.
-//  Copyright © 2019 John Garrett. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
